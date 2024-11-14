b38cad56ec052975ad68cf29f3b5451891f32cc1
s4031499 <s4031499@rmit.edu.vn>
2024-11-14 18:06:32 +0700

    second

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <meta name="apple-mobile-web-app-title" content="Hewllo" />
+        <link rel="apple-touch-icon" href="/path/to/apple-icon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
