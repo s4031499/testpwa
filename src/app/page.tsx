e2099d4986001e48ac9414a09d56281c0ebff76a
s4031499 <s4031499@rmit.edu.vn>
2024-11-14 18:32:23 +0700

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -150,7 +150,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Thay doi
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
